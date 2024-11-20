a079882d9f27ac27498039c831658eae90a01a20
loussalmohammed <hplaptopsimo@gmail.com>
2024-11-20 14:46:21 +0100

    add base generic service

---
+++ src/main/java/org/CitronixFarm/citronix/common/service/BaseService.java
@@ -0,0 +1,13 @@
+package org.CitronixFarm.citronix.common.service;
+
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+import org.wrmList.waitingList.shared.dto.PageDTO;
+
+public interface BaseService<Long, C, U, R> {
+    R create(@Valid C requestDTO);
+    PageDTO<R> findAll(Pageable pageable);
+    R findById(Long id);
+    R updateById(U updateDTO, Long id);
+    void deleteById(Long id);
+}

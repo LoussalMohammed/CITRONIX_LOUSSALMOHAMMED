ab0ee30b99b5a54f872097610adae8f207e449b1
loussalmohammed <hplaptopsimo@gmail.com>
2024-11-24 19:39:20 +0100

    add find farm with crieteria builder

--- src/main/java/org/CitronixFarm/citronix/farm/service/FarmService.java
+++ src/main/java/org/CitronixFarm/citronix/farm/service/FarmService.java
@@ -1,11 +1,18 @@
-package org.CitronixFarm.citronix.Farm.service;
+package org.CitronixFarm.citronix.farm.service;
 
-import org.CitronixFarm.citronix.Farm.dto.request.CreateFarmDTO;
-import org.CitronixFarm.citronix.Farm.dto.request.UpdateFarmDTO;
-import org.CitronixFarm.citronix.Farm.dto.response.ResponseFarmDTO;
+import org.CitronixFarm.citronix.farm.dto.request.CreateFarmDTO;
+import org.CitronixFarm.citronix.farm.dto.request.FarmFilterDTO;
+import org.CitronixFarm.citronix.farm.dto.request.UpdateFarmDTO;
+import org.CitronixFarm.citronix.farm.dto.response.ResponseFarmDTO;
 import org.CitronixFarm.citronix.common.service.BaseService;
+import org.CitronixFarm.citronix.farm.entity.Farm;
+import org.CitronixFarm.citronix.shared.dto.PageDTO;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface FarmService extends BaseService<Long, CreateFarmDTO, UpdateFarmDTO, ResponseFarmDTO> {
+    public Specification<Farm> createSpecification(FarmFilterDTO filterDTO);
+    PageDTO<ResponseFarmDTO> findAllWithFilters(FarmFilterDTO filterDTO, Pageable pageable);
 }
